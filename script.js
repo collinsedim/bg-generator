f53655de00546fed0d404490eca740af3f48f546
dlagunfry <collinsedim@gmail.com>
2023-03-26 13:43:29 +0100

    updated variables

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 function selectColor() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
