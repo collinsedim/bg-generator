b10f4953ed7eb663d9de92c0662ddfaf0c587e69
dlagunfry <collinsedim@gmail.com>
2023-03-26 14:35:26 +0100

    updated css and JS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
+const aEL = addEventListener("input", selectColor);
 
 function selectColor() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
@@ -9,6 +10,5 @@ function selectColor() {
     css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", selectColor);
-
-color2.addEventListener("input", selectColor);
+color1.aEL;
+color2.aEl;
